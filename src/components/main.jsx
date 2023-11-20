2933021c94ede427f4c3ef72b85724e04557f37c
Catherine <z5359881@student.unsw.edu.au>
2023-11-20 15:09:57 +1100

    removed use of localstorage for input name text

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
 const MainPage = () => {
   // const [success, setSuccess] = useState(true);
-  const [storedName, setStoredName] = useState(localStorage.getItem('name'));
   const { register, handleSubmit } = useForm();
   const navigate = useNavigate();
 
@@ -13,27 +12,17 @@ const MainPage = () => {
       // setSuccess(true);
       navigate('/counter', { state: { name: data.name } });
       localStorage.setItem('name', JSON.stringify(data.name));
-      setStoredName(JSON.stringify(data.name));
     } else {
       // setSuccess(false);
       alert('Please enter your name');
     }
   }
 
-  useEffect(() => {
-    const name = localStorage.getItem('name');
-    let finalName = '';
-    if (name) {
-      const finalName = name.substring(1, name.length - 1);
-      setStoredName(finalName);
-    }
-  }, [])
-
   return (
     <header className="App-header">
       <h1>Counter</h1>
       <form onSubmit={handleSubmit(onSubmission)}>
-        <input onChange={(e) => setStoredName(e.target.value)} placeholder="Name" {...register("name")}></input>
+        <input placeholder="Name" {...register("name")}></input>
         <button type='submit'> Click me! </button>
       </form>
     </header>
